4982253047c6b7daace2ca2d75a904cb5efad2d6
parth258 <parthsucklecha25@gmail.com>
2026-08-29 16:29:13 +0530

    Fix MongoDB SSL handshake on Render

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter, HTTPException
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
+import certifi
 import os
 import asyncio
 import html as html_lib
@@ -18,7 +19,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 
 app = FastAPI()
